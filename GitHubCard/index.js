bb162edcfb68fe1f295ae784e68af0de4de28ec8
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2019-12-15 14:49:08 -0500

    Auto Cards for Followers

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -9,10 +9,93 @@
 
    Skip to Step 3.
 */
+const cardContainer = document.querySelector(".cards")
+axios
+  .get("https://api.github.com/users/Benji171")
 
+  .then((res) => {
+    console.log(res);
+
+    cardContainer.append(component(res))
+    })
+  .catch((err) => {
+    console.log(`You hit an error: ${err}`);
+  });
+
+  
+const followersArray = [];
+
+axios
+    .get('https://api.github.com/users/Benji171/followers')
+    .then((res) => {
+        const followersArray = res.data;
+        const list = followersArray.map((user) => {
+            return user.login;
+        });
+        const followersList = list.slice(0,7)
+        return followersList;
+    })
+    .then((followersList) => {
+        followersList.forEach((user) => {
+            axios.get(`https://api.github.com/users/${user}`)
+            .then((res) => {
+              console.log(res);
+
+              cardContainer.append(component(res))
+            });
+        });
+    })
+    .catch((err) => console.log(err));
+
+
+function component(obj) {
+  const card = document.createElement("div")
+  const img = document.createElement("img")
+  const cardInfo = document.createElement("div")
+  const name = document.createElement("h3")
+  const usrName = document.createElement("p")
+  const location = document.createElement("p")
+  const profile = document.createElement("p")
+  const a = document.createElement("a")
+  const followers = document.createElement("p")
+  const following = document.createElement("p")
+  const bio = document.createElement("p")
+
+  card.appendChild(img)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(usrName)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+  profile.appendChild(a)
+
+  img.src = obj.data.avatar_url;
+  name.textContent = obj.data.name;
+  usrName.textContent = obj.data.login;
+  location.textContent= `Location: ${obj.data.location}`
+  profile.textContent= `Profile:`
+  a.href = obj.data.html_url
+  a.textContent = obj.data.html_url
+  followers.textContent = `Followers: ${obj.data.followers}`
+  following.textContent = `Following: ${obj.data.following}`
+  bio.textContent = `Bio: ${obj.data.bio}`
+
+  card.classList.add("card")
+  cardInfo.classList.add("card-info")
+  name.classList.add("name")
+  usrName.classList.add("username")
+
+  return card
+}
+
+console.log(followersArray)
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -24,7 +107,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
